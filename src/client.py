2c0ea54e4add1a3247f19511a4477a8d4d56a4c2
Daniel Ranchal <daniranchal@wealize.digital>
2021-05-19 21:19:26 +0200

    Making improvements

--- src/client.py
+++ src/client.py
@@ -1,34 +1,27 @@
 import json
+from dataclasses import asdict
+from typing import Dict, List, Optional, Tuple, Union
 
-from typing import List, Union
 import requests
 from requests.models import HTTPError
 
 from .exceptions import NotionException
 from .constants import API_ENDPOINT, API_VERSION
+from .types.database import Database
 from .types.errors import NotionError
+from .types.misc import QueryParams
 from .types.users import User
 
 
 class NotionClient:
     def __init__(self, token: str) -> None:
         self.users = UserEndpoint(token)
+        self.databases = DatabaseEndoint(token)
 
 
-class UserEndpoint:
-    def __init__(self, token) -> None:
-        self.user_endpoint = f'{API_ENDPOINT}/users/'
-        self.token = token
-
-    def perform_request(self, method: str, endpoint: str):
-        response = requests.request(
-            method=method,
-            url=endpoint,
-            headers={
-                'Authorization': f'Bearer {self.token}',
-                'Notion-Version': API_VERSION
-            }
-        )
+class GenericEndpoint:
+    def perform_request(self, request_data: Dict) -> Dict:
+        response = requests.request(**request_data)
 
         try:
             response.raise_for_status()
@@ -37,22 +30,87 @@ class UserEndpoint:
 
         return response.json()
 
-    def get(self, user_id: str) -> Union[User, NotionError]:
-        endpoint = f'{self.user_endpoint}{user_id}'
+class DatabaseEndoint(GenericEndpoint):
+    def __init__(self, token: str) -> None:
+        self.database_endpoint: str = f'{API_ENDPOINT}/databases/'
+        self.token: str = token
+
+    def get(self, database_id: str) -> Union[Database, NotionError]:
+        request_data = {
+            'method': 'GET',
+            'url': f'{self.database_endpoint}{database_id}',
+            'headers': {
+                'Authorization': f'Bearer {self.token}',
+                'Notion-Version': API_VERSION
+            }
+        }
 
         try:
-            response = self.perform_request('GET', endpoint)
+            response = self.perform_request(request_data)
+        except NotionException as e:
+            return NotionError(e.code, e.message)
+
+        return Database(**response)
+
+    def list(self, query_params: QueryParams) -> Union[Tuple[List[Database],Optional[str]], NotionError]:
+        request_data = {
+            'method': 'GET',
+            'url': f'{self.database_endpoint}',
+            'params': asdict(query_params),
+            'headers': {
+                'Authorization': f'Bearer {self.token}',
+                'Notion-Version': API_VERSION
+            }
+        }
+
+        try:
+            response = self.perform_request(request_data)
+        except NotionException as e:
+            return NotionError(e.code, e.message)
+
+        next_cursor = response['next_cursor'] if response['has_more'] else None
+
+        return [Database(**item) for item in response['results']], next_cursor
+
+
+class UserEndpoint(GenericEndpoint):
+    def __init__(self, token) -> None:
+        self.user_endpoint = f'{API_ENDPOINT}/users/'
+        self.token = token
+
+    def get(self, user_id: str) -> Union[User, NotionError]:
+        request_data = {
+            'method': 'GET',
+            'url': f'{self.user_endpoint}{user_id}',
+            'headers': {
+                'Authorization': f'Bearer {self.token}',
+                'Notion-Version': API_VERSION
+            }
+        }
+
+        try:
+            response = self.perform_request(request_data)
         except NotionException as e:
             return NotionError(e.code, e.message)
 
         return User(**response)
 
-    def list(self) -> Union[List[User], NotionError]:
-        endpoint = f'{self.user_endpoint}'
+    def list(self, query_params: QueryParams) -> Union[Tuple[List[User],Optional[str]], NotionError]:
+        request_data = {
+            'method': 'GET',
+            'url': f'{self.user_endpoint}',
+            'params': asdict(query_params),
+            'headers': {
+                'Authorization': f'Bearer {self.token}',
+                'Notion-Version': API_VERSION
+            }
+        }
 
         try:
-            response = self.perform_request('GET', endpoint)
+            response = self.perform_request(request_data)
         except NotionException as e:
             return NotionError(e.code, e.message)
 
-        return [User(**item) for item in response['results']] 
+        next_cursor = response['next_cursor'] if response['has_more'] else None
+
+        return [User(**item) for item in response['results']], next_cursor
--- src/types/misc.py
+++ src/types/misc.py
@@ -8,3 +8,13 @@ class DateProperty:
 
     def is_date_range(self):
         return not self.end
+
+
+@dataclass
+class QueryParams:
+    start_cursor: str
+    page_size: int
+
+    def __post_init__(self):
+        if self.page_size >= 100:
+            self.page_size = 100
